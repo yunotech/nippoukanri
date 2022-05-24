3dab5856571d486196a0d066246a5d79df21bbdb
wid_ds_training <wid_ds_training@QAW402>
2022-05-24 15:17:11 +0900

    Employeeモデル、EmployeeViewモデル作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -1,71 +1,74 @@
 package constants;
 
-//画面の項目値等を定義するEnumクラス
+/**
+ * 画面の項目値等を定義するEnumクラス
+ *
+ */
+public enum AttributeConst {
 
-public enum AttributeConst{
+    //フラッシュメッセージ
+    FLUSH("flush"),
 
-	//フラッシュメッセージ
-	FLUSH("flush"),
+    //一覧画面共通
+    MAX_ROW("maxRow"),
+    PAGE("page"),
 
-	//一覧画面共通
-	MAX_ROW("maxRow"),
-	PAGE("page"),
+    //入力フォーム共通
+    TOKEN("_token"),
+    ERR("errors"),
 
-	//入力フォーム共通
-	TOKEN("_token"),
-	ERR("errors"),
+    //ログイン中の従業員
+    LOGIN_EMP("login_employee"),
 
-	//ログイン中の従業員
-	LOGIN_EMP("login_employee"),
+    //ログイン画面
+    LOGIN_ERR("loginError"),
 
-	//ログイン画面
-	LOGIN_ERR("loginError"),
+    //従業員管理
+    EMPLOYEE("employee"),
+    EMPLOYEES("employees"),
+    EMP_COUNT("employees_count"),
+    EMP_ID("id"),
+    EMP_CODE("code"),
+    EMP_PASS("password"),
+    EMP_NAME("name"),
+    EMP_ADMIN_FLG("admin_flag"),
 
-	//従業員管理
-	EMPLOYEE("employee"),
-	EMPLOYEES("employees"),
-	EMP_COUNT("employee_count"),
-	EMP_ID("id"),
-	EMP_CODE("code"),
-	EMP_PASS("password"),
-	EMP_NAME("name"),
-	EMP_ADMIN_FLG("admin_flag"),
+    //管理者フラグ
+    ROLE_ADMIN(1),
+    ROLE_GENERAL(0),
 
-	//管理者フラグ
-	ROLE_ADMIN(1),
-	ROLE_GENERAL(0),
+    //削除フラグ
+    DEL_FLAG_TRUE(1),
+    DEL_FLAG_FALSE(0),
 
-	//削除フラグ
-	DEL_FLAG_TRUE(1),
-	DEL_FLAG_FALSE(0),
+    //日報管理
+    REPORT("report"),
+    REPORTS("reports"),
+    REP_COUNT("reports_count"),
+    REP_ID("id"),
+    REP_DATE("report_date"),
+    REP_TITLE("title"),
+    REP_CONTENT("content");
 
-	//日報管理
-	REPORT("report"),
-	REPORTS("reports"),
-	REP_COUNT("reports_count"),
-	REP_ID("id"),
-	REP_DATE("report_date"),
-	REP_TITLE("title"),
-	REP_CONTENT("content");
+    private final String text;
+    private final Integer i;
 
-	private final String text;
-	private final Integer i;
+    private AttributeConst(final String text) {
+        this.text = text;
+        this.i = null;
+    }
 
-	private AttributeConst(final String text){
-	this.text = text;
-	this.i = null;
-	}
+    private AttributeConst(final Integer i) {
+        this.text = null;
+        this.i = i;
+    }
 
-	private AttributeConst(final Integer i){
-	this.text = null;
-	this.i = i;
-	}
+    public String getValue() {
+        return this.text;
+    }
 
-	public String getValue(){
-	return this.text;
-	}
+    public Integer getIntegerValue() {
+        return this.i;
+    }
 
-	public Integer getIntegerValue(){
-	return this.i;
-	}
 }
