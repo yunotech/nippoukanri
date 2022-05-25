929f31830d8440bbd73498341e130ba44e316f92
wid_ds_training <wid_ds_training@QAW402>
2022-05-25 11:09:29 +0900

    ReportValidator、ReportService作成

--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -11,147 +11,150 @@ import constants.JpaConst;
 import models.Report;
 import models.validators.ReportValidator;
 
-/**
- * 日報テーブルの操作に関わる処理を行うクラス
- */
-public class ReportService extends ServiceBase {
+//日報テーブルの操作に関わる処理を行うクラス
+public class ReportService extends ServiceBase{
 
-    /**
-     * 指定した従業員が作成した日報データを、指定されたページ数の一覧画面に表示する分取得しReportViewのリストで返却する
-     * @param employee 従業員
-     * @param page ページ数
-     * @return 一覧画面に表示するデータのリスト
-     */
-    public List<ReportView> getMinePerPage(EmployeeView employee, int page) {
+	/*
+	 * 指定した従業員が作成した日報データを指定されたページ数の一覧画面に表示する分取得し、ReportViewのリストで返す
+	 * @param emoloyee 従業員
+	 * @param page ページ数
+	 * @return 一覧画面に表示するデータのリスト
+	 */
+	public List<ReportView> getMinePerPage(EmployeeView employee,int page){
 
-        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE, Report.class)
-                .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
-                .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
-                .setMaxResults(JpaConst.ROW_PER_PAGE)
-                .getResultList();
-        return ReportConverter.toViewList(reports);
-    }
+		List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE, Report.class)
+				.setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
+				.setFirstResult(JpaConst.ROW_PER_PAGE * (page -1))
+				.setMaxResults(JpaConst.ROW_PER_PAGE)
+				.getResultList();
+		return ReportConverter.toViewList(reports);
+	}
 
-    /**
-     * 指定した従業員が作成した日報データの件数を取得し、返却する
-     * @param employee
-     * @return 日報データの件数
-     */
-    public long countAllMine(EmployeeView employee) {
+	/*
+	 * 指定した従業員が作成した日報データの件数を取得し返す
+	 * @param employee
+	 * @return 日報データの件数
+	 */
+	public long countAllMine(EmployeeView employee) {
+		long count = (long)em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
+				.setParameter(JpaConst.JPQL_PARM_EMPLOYEE,EmployeeConverter.toModel(employee))
+				.getSingleResult();
 
-        long count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
-                .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
-                .getSingleResult();
+		return count;
+	}
 
-        return count;
-    }
+	/*
+	 * 指定されたページ数の一覧画面に表示する日報データを取得し、ReportViewのリストで返す
+	 * @param page ページ数
+	 * @return 一覧画面に表示するデータのリスト
+	 */
+	public List<ReportView> getAllPerPage(int page){
 
-    /**
-     * 指定されたページ数の一覧画面に表示する日報データを取得し、ReportViewのリストで返却する
-     * @param page ページ数
-     * @return 一覧画面に表示するデータのリスト
-     */
-    public List<ReportView> getAllPerPage(int page) {
+		List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL,Report.class)
+				.setFirstResult(JpaConst.ROW_PER_PAGE * (page -1))
+				.setMaxResults(JpaConst.ROW_PER_PAGE)
+				.getResultList();
+		return ReportConverter.toViewList(reports);
+	}
+	/*
+	 * 日報テーブルのデータの件数を取得し、返す
+	 * @return データの件数
+	 */
+	public long countAll() {
+		long reports_count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT, Long.class)
+				.getSingleResult();
+		return reports_count;
+	}
+	/*
+	 * idを条件に取得したデータをReportViewのインスタンスで返す
+	 * @param id
+	 * @return 取得データのインスタンス
+	 */
+	public ReportView findOne(int id) {
+		return ReportConverter.toView(findOneInternal(id));
+	}
+	/*
+	 * 画面から入力された日報の登録内容をもとにデータを1件作成し、日報テーブルに登録する
+	 * @param rv 日報の登録
+	 * @return バリデーションで発生したエラーのリスト
+	 */
+	public List<String> create(ReportView rv){
+		List<String> errors = ReportValidator.validate(rv);
+		if(errors.size() == 0) {
+			LocalDateTime ldt = LocalDateTime.now();
+			rv.setCreatedAt(ldt);
+			rv.setUpdatedAt(ldt);
+			createInternal(rv);
+		}
+		//バリデーションで発生したエラーを返す（エラーがなければ0件の空リスト）
+		return errors;
+	}
+	/*
+	 * 画面から入力された日報の登録内容を元に、日報データを更新する
+	 * @param rv 日報の更新情報
+	 * @return バリデーションで発生したエラーのリスト
+	 */
+	public List<String> update(ReportView rv){
 
-        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL, Report.class)
-                .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
-                .setMaxResults(JpaConst.ROW_PER_PAGE)
-                .getResultList();
-        return ReportConverter.toViewList(reports);
-    }
+		//バリデーションを行う
+		List<String> errors = ReportValidator.validate(rv);
 
-    /**
-     * 日報テーブルのデータの件数を取得し、返却する
-     * @return データの件数
-     */
-    public long countAll() {
-        long reports_count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT, Long.class)
-                .getSingleResult();
-        return reports_count;
-    }
+		if(errors.size() == 0) {
 
-    /**
-     * idを条件に取得したデータをReportViewのインスタンスで返却する
-     * @param id
-     * @return 取得データのインスタンス
-     */
-    public ReportView findOne(int id) {
-        return ReportConverter.toView(findOneInternal(id));
-    }
+			//更新日時を現在時刻に設定
+			LocalDateTime ldt = LocalDateTime.now();
+			rv.setUpdatedAt(ldt);
 
-    /**
-     * 画面から入力された日報の登録内容を元にデータを1件作成し、日報テーブルに登録する
-     * @param rv 日報の登録内容
-     * @return バリデーションで発生したエラーのリスト
-     */
-    public List<String> create(ReportView rv) {
-        List<String> errors = ReportValidator.validate(rv);
-        if (errors.size() == 0) {
-            LocalDateTime ldt = LocalDateTime.now();
-            rv.setCreatedAt(ldt);
-            rv.setUpdatedAt(ldt);
-            createInternal(rv);
-        }
+			updateInternal(rv);
+		}
 
-        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
-        return errors;
-    }
+		//バリデーションで発生したエラーを返す（エラーがなければ0件の空リスト）
+		return errors;
+	}
 
-    /**
-     * 画面から入力された日報の登録内容を元に、日報データを更新する
-     * @param rv 日報の更新内容
-     * @return バリデーションで発生したエラーのリスト
-     */
-    public List<String> update(ReportView rv) {
+	/*
+	 * idを条件にデータを1件取得する
+	 * @param id
+	 * @return 取得データのインスタンス
+	 */
 
-        //バリデーションを行う
-        List<String> errors = ReportValidator.validate(rv);
+	private Report findOneInternal(int id) {
+		return em.find(Report.class,id);
+	}
 
-        if (errors.size() == 0) {
+	/*
+	 * 日報データを1件登録する
+	 * @param rv 日報データ
+	 */
+	private void createInternal(ReportView rv) {
 
-            //更新日時を現在時刻に設定
-            LocalDateTime ldt = LocalDateTime.now();
-            rv.setUpdatedAt(ldt);
+		em.getTransaction().begin();
+		em.persist(ReportConverter.toModel(rv));
+		em.getTransaction().commit();
+	}
 
-            updateInternal(rv);
-        }
+	/*
+	 * 日報データを更新する
+	 * @param rv 日報データ
+	 */
 
-        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
-        return errors;
-    }
+	private void updateInternal(ReportView rv) {
 
-    /**
-     * idを条件にデータを1件取得する
-     * @param id
-     * @return 取得データのインスタンス
-     */
-    private Report findOneInternal(int id) {
-        return em.find(Report.class, id);
-    }
+		em.getTransaction().begin();
+		Report r = findOneInternal(rv.getId());
+		ReportConverter.CopyViewToModel(r, rv);
+		em.getTransaction().commit();
+	}
 
-    /**
-     * 日報データを1件登録する
-     * @param rv 日報データ
-     */
-    private void createInternal(ReportView rv) {
-
-        em.getTransaction().begin();
-        em.persist(ReportConverter.toModel(rv));
-        em.getTransaction().commit();
-
-    }
-
-    /**
-     * 日報データを更新する
-     * @param rv 日報データ
-     */
-    private void updateInternal(ReportView rv) {
-
-        em.getTransaction().begin();
-        Report r = findOneInternal(rv.getId());
-        ReportConverter.copyViewToModel(r, rv);
-        em.getTransaction().commit();
-
-    }
 
 }
+
+
+
+
+
+
+
+
+
+
