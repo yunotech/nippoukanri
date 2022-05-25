873addda3518023d3823da520f81efdd97e177f7
wid_ds_training <wid_ds_training@QAW402>
2022-05-25 11:27:26 +0900

    日報indexアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -11,55 +11,48 @@ import constants.ForwardConst;
 import constants.JpaConst;
 import services.ReportService;
 
-/**
- * 日報に関する処理を行うActionクラス
- *
- */
-public class ReportAction extends ActionBase {
+//日報に関する処理を行うActionクラス
 
-    private ReportService service;
+public class ReportAction extends ActionBase{
 
-    /**
-     * メソッドを実行する
-     */
-    @Override
-    public void process() throws ServletException, IOException {
+	private ReportService service;
 
-        service = new ReportService();
+	//メソッドを実行する
 
-        //メソッドを実行
-        invoke();
-        service.close();
-    }
+	@Override
+	public void process() throws ServletException , IOException{
 
-    /**
-     * 一覧画面を表示する
-     * @throws ServletException
-     * @throws IOException
-     */
-    public void index() throws ServletException, IOException {
+		service = new ReportService();
 
-        //指定されたページ数の一覧画面に表示する日報データを取得
-        int page = getPage();
-        List<ReportView> reports = service.getAllPerPage(page);
+		//メソッドを実行
+		invoke();
+		service.close();
+	}
+	/*一覧画面を表示する
+	 * @throws ServletException
+	 * @throws IOException
+	*/
+	public void index() throws ServletException, IOException{
+		//指定されたページの一覧画面に表示する日報データを取得
+		int page = getPage();
+		List<ReportView> reports = service.getAllPerPage(page);
 
-        //全日報データの件数を取得
-        long reportsCount = service.countAll();
+		//全日報データの件数を取得
+		long reportsCount = service.countAll();
 
-        putRequestScope(AttributeConst.REPORTS, reports); //取得した日報データ
-        putRequestScope(AttributeConst.REP_COUNT, reportsCount); //全ての日報データの件数
-        putRequestScope(AttributeConst.PAGE, page); //ページ数
-        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+		putRequestScope(AttributeConst.REPORTS,reports);//取得した日報データ
+		putRequestScope(AttributeConst.REP_COUNT,reportsCount);//全ての日報データの件数
+		putRequestScope(AttributeConst.PAGE,page);//ページ数
+		putRequestScope(AttributeConst.MAX_ROW,JpaConst.ROW_PER_PAGE);//1ページに表示するレコードの数
 
-        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
-        String flush = getSessionScope(AttributeConst.FLUSH);
-        if (flush != null) {
-            putRequestScope(AttributeConst.FLUSH, flush);
-            removeSessionScope(AttributeConst.FLUSH);
-        }
+		//セッションにフラッシュメッセージが設定されている場合はリクエストスコープに差し替え、セッションからは削除する
+		String flush = getSessionScope(AttributeConst.FLUSH);
+		if(flush != null) {
+			putRequestScope(AttributeConst.FLUSH,flush);
+			removeSessionScope(AttributeConst.FLUSH);
+		}
 
-        //一覧画面を表示
-        forward(ForwardConst.FW_REP_INDEX);
-    }
-
+		//一覧画面を表示
+		forward(ForwardConst.FW_REP_INDEX);
+	}
 }
