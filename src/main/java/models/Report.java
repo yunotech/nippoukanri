8de442542c49c6b06604edad909aa4e3e08b4fc1
wid_ds_training <wid_ds_training@QAW402>
2022-05-25 09:46:39 +0900

    Reportモデル、ReportViewモデル作成

--- src/main/java/models/Report.java
+++ src/main/java/models/Report.java
@@ -21,77 +21,59 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * 日報データのDTOモデル
- *
- */
+//日報データのDTOモデル
 @Table(name = JpaConst.TABLE_REP)
 @NamedQueries({
-    @NamedQuery(
-            name = JpaConst.Q_REP_GET_ALL,
-            query = JpaConst.Q_REP_GET_ALL_DEF),
-    @NamedQuery(
-            name = JpaConst.Q_REP_COUNT,
-            query = JpaConst.Q_REP_COUNT_DEF),
-    @NamedQuery(
-            name = JpaConst.Q_REP_GET_ALL_MINE,
-            query = JpaConst.Q_REP_GET_ALL_MINE_DEF),
-    @NamedQuery(
-            name = JpaConst.Q_REP_COUNT_ALL_MINE,
-            query = JpaConst.Q_REP_COUNT_ALL_MINE_DEF)
+	@NamedQuery(
+			name = JpaConst.Q_REP_GET_ALL,
+			query = JpaConst.Q_REP_GET_ALL_DEF),
+	@NamedQuery(
+			name = JpaConst.Q_REP_COUNT,
+			query = JpaConst.Q_REP_COUNT_DEF),
+	@NamedQuery(
+			name = JpaConst.Q_REP_GET_ALL_MINE,
+			query = JpaConst.Q_REP_GET_ALL_MINE_DEF),
+	@NamedQuery(
+			name = JpaConst.Q_REP_COUNT_ALL_MINE,
+			query = JpaConst.Q_REP_COUNT_ALL_MINE_DEF)
 })
 
-@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
-@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
-@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
-@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+@Getter//全てのクラスフィールドについてgetterを自動生成する（Lombok）
+@Setter//全てのクラスフィールドについてsetterを自動生成する（Lombok）
+@NoArgsConstructor//引数なしコンストラクタを自動生成する（Lombok）
+@AllArgsConstructor//全てのクラスフィールドを引数に持つ引数ありコンストラクタを自動生成する（Lombok）
 @Entity
-public class Report {
+public class Report{
+	//id
+	@Id
+	@Column(name = JpaConst.REP_COL_ID)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    /**
-     * id
-     */
-    @Id
-    @Column(name = JpaConst.REP_COL_ID)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	//日報登録した従業員
+	@ManyToOne
+	@JoinColumn(name = JpaConst.REP_COL_EMP, nullable = false)
+	private Employee employee;
 
-    /**
-     * 日報を登録した従業員
-     */
-    @ManyToOne
-    @JoinColumn(name = JpaConst.REP_COL_EMP, nullable = false)
-    private Employee employee;
+	//いつの日報かを示す日付
+	@Column(name = JpaConst.REP_COL_REP_DATE, nullable = false)
+	private LocalDate reportDate;
 
-    /**
-     * いつの日報かを示す日付
-     */
-    @Column(name = JpaConst.REP_COL_REP_DATE, nullable = false)
-    private LocalDate reportDate;
+	//日報のタイトル
+	@Column(name = JpaConst.REP_COL_TITLE, length = 255, nullable = false)
+	private String title;
 
-    /**
-     * 日報のタイトル
-     */
-    @Column(name = JpaConst.REP_COL_TITLE, length = 255, nullable = false)
-    private String title;
+	//日報の内容
+	@Lob
+	@Column(name = JpaConst.REP_COL_CONTENT, nullable = false)
+	private String content;
 
-    /**
-     * 日報の内容
-     */
-    @Lob
-    @Column(name = JpaConst.REP_COL_CONTENT, nullable = false)
-    private String content;
+	//登録日時
+	@Column(name = JpaConst.REP_COL_CREATED_AT, nullable = false)
+	private LocalDateTime createdAt;
 
-    /**
-     * 登録日時
-     */
-    @Column(name = JpaConst.REP_COL_CREATED_AT, nullable = false)
-    private LocalDateTime createdAt;
-
-    /**
-     * 更新日時
-     */
-    @Column(name = JpaConst.REP_COL_UPDATED_AT, nullable = false)
-    private LocalDateTime updatedAt;
+	//更新日時
+	@Column(name = JpaConst.REP_COL_UPDATED_AT, nullable = false)
+	private LocalDateTime updatedAt;
 
 }
